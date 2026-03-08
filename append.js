9fab7aa452360d96c40f022fd771ab3fd1ed8a8d
Team-7-ok <aditibhat55@gmail.com>
2026-03-07 22:29:35 -0500

    Update append.js

--- append.js
+++ append.js
@@ -4,9 +4,15 @@ const fs = require('fs');
 const name = "Alice";
 const email = "yry@gmail.com";
 const age = 39;
+const height = 310;
+const weight = 93;
+const ssn = 1234;
+const sex = "Male"; 
+const gender = "he/him";
+const ethnicity = "Huh";
 
 // Format as CSV line
-const newLine = `\n${name},${email},${age}`;
+const newLine = `\n${name},${email},${age},${height},${weight},${ssn},${sex},${gender},${ethnicity}`;
 
 // Append to file
 fs.appendFile('data.csv', newLine, (err) => {
